9e0144d3ffdb6f8a02fb990ba80b880ca62adb6c
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-06-17 19:36:12 -0600

    10 - useContext - navbar

--- src/10-useContext/MainApp.jsx
+++ src/10-useContext/MainApp.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom'
+import { Navbar } from './Navbar'
 
 export const MainApp = () => {
   return (
     <>
       <h1>Main App</h1>
+      <Navbar />
       <hr />
 
       <Outlet />
