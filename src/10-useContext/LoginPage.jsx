b4b321534705fbed2387debf1fb5104de9e5b94f
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-06-17 20:02:57 -0600

    10 - useContext - using context

--- src/10-useContext/LoginPage.jsx
+++ src/10-useContext/LoginPage.jsx
@@ -1,8 +1,28 @@
+import { useContext } from 'react'
+import { UserContext } from './context/UserContext'
+
 export const LoginPage = () => {
+  const { user, setUser } = useContext(UserContext)
+
   return (
     <>
-      <h1>Login Page</h1>
+      <h1>Login Page {user.name}</h1>
       <hr />
+
+      <pre>{JSON.stringify(user)}</pre>
+
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          setUser({
+            name: 'otheruser',
+            email: 'yo@email.com',
+            id: 12342,
+          })
+        }}
+      >
+        Set user
+      </button>
     </>
   )
 }
